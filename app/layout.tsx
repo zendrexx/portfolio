22026630024745487643c2e010af78be5ddce405
Zendrex <162876828+zendrexx@users.noreply.github.com>
2026-07-24 11:08:09 +0800

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
-
+//new ver
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
